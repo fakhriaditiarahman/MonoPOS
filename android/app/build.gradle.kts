d3f6ee0b17d2497ee1f2b0883ea08b679ac5fefd
Fakhri Aditia Rahman <fakhri@student.unp.ac.id>
2026-09-08 18:54:14 +0700

    chore: bump version to v1.2.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.monodev.monopos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.2.2"
+        versionCode = 13
+        versionName = "1.2.3"
         multiDexEnabled = true
     }
 
